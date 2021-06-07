6ad1297f1cc7af798e1a4eedf802676d74d06fbd
Tyler Jachetta <tjachetta@imgix.com>
2021-06-07 11:40:22 -0400

    Added python stubs for beginning of plugin

---
+++ src/tox_in_docker/main.py
@@ -0,0 +1,28 @@
+
+import docker
+import os
+
+MOUNT_POINT = '/testing-ro'
+TEST_DIR = '/testing'
+
+HERE_MOUNT = {
+    os.getcwd(): {
+        'bind': MOUNT_POINT
+    }
+}
+
+def do_tests():
+    cmd = f'bash -c "cp -r {MOUNT_POINT} {TEST_DIR};cd {TEST_DIR}; tox"'
+
+    do_launch(cmd)
+
+# TOREMOVE: these kwargs are definitely not for realz
+def do_launch(cmd, image='ubuntu'):
+
+    client = docker.client.DockerClient()
+
+    return(client.containers.run(image, cmd, volumes=HERE_MOUNT))
+
+
+def hacky_test():
+    return 'foo'
