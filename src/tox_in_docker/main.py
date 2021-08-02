7ffd971f31135a9e5b67aac8e0942f30293c3cde
Tyler Jachetta <tjachetta@imgix.com>
2021-08-02 17:09:44 -0600

    Bunch of work on the actual test running

--- src/tox_in_docker/main.py
+++ src/tox_in_docker/main.py
@@ -1,6 +1,7 @@
 
 import docker
 import os
+import shlex
 
 MOUNT_POINT = '/testing-ro'
 TEST_DIR = '/testing'
@@ -11,15 +12,36 @@ HERE_MOUNT = {
     }
 }
 
-def do_tests():
-    cmd = f'bash -c "cp -r {MOUNT_POINT} {TEST_DIR};cd {TEST_DIR}; tox"'
 
-    do_launch(cmd)
-
-# TOREMOVE: these kwargs are definitely not for realz
-def do_launch(cmd, image='ubuntu'):
+def run_tests(images=None):
+    if images is None:
+        images = ['python:latest']
 
     client = docker.client.DockerClient()
+    cmd = get_test_cmd()
 
-    return(client.containers.run(image, cmd, volumes=HERE_MOUNT))
+    results = {}
+
+    # ToDo entrypoint?
+    for image in images:
+        results[image] = client.containers.run(image, cmd, volumes=HERE_MOUNT)
+
+    return results
+
+
+def get_test_cmd():
+
+    cmds = [
+        ['pip', 'install', '--no-input', 'tox'],
+        ['cp', '-r', MOUNT_POINT, TEST_DIR],
+        ['cd', TEST_DIR],
+        ['tox']
+    ]
+
+    bash_cmd = ';'.join(shlex.join(cmd) for cmd in cmds)
+
+    full_cmd = shlex.join(['bash', '-c', bash_cmd])
+
+    return full_cmd
+
 
