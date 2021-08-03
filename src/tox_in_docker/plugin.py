bbb927d2dc25b4ba7f29eb3d9973c1c6a2f28b7a
Tyler Jachetta <tjachetta@imgix.com>
2021-08-02 18:39:11 -0600

    Setup plugin to handle ignoring the plugin. yo.

--- src/tox_in_docker/plugin.py
+++ src/tox_in_docker/plugin.py
@@ -12,8 +12,14 @@ hookimpl = pluggy.HookimplMarker("tox")
 @hookimpl
 def tox_addoption(parser: tox.config.Parser):
     """Add a command line option for later use"""
-    parser.add_argument("--no_tox_in_docker", action='store_false', dest='use_docker',
+    parser.add_argument("--no_tox_in_docker", action='store_false', dest='in_docker',
                         help="disable this plugin")
+
+    parser.add_testenv_attribute(
+        name="in_docker",
+        type="bool",
+        help="set `true` to use tox-in-docker"
+    )
 
     parser.add_testenv_attribute(
         name="docker_images",
@@ -25,12 +31,21 @@ def tox_addoption(parser: tox.config.Parser):
     )
 
 
+
+def _skip_this_perhaps(venv):
+    if not venv.envconfig.config.option.in_docker:
+        return True
+
+    if not venv.envconfig.in_docker:
+        return True
+
+
 @hookimpl
 def tox_configure(config: tox.config.Config):
     """Access your option during configuration"""
     # verbosity0("flag magic is: {}".format(config.option.magic))
 
-    if not config.option.use_docker:
+    if not config.option.in_docker:
         return None
 
     # TODO
@@ -39,18 +54,27 @@ def tox_configure(config: tox.config.Config):
 
 @hookimpl
 def tox_testenv_create(venv: tox.venv.VirtualEnv, action):
-    if not venv.envconfig.config.option.use_docker:
+
+    if _skip_this_perhaps(venv):
         return None
+
     if venv.envconfig.envname.startswith('.'):
         # Not sure if this is too prescriptive
         return None
+
 
 @hookimpl
 def tox_runtest_post(venv: tox.venv.VirtualEnv):
     # Options (like `config.option` in `tox_configure`) are at
     # `venv.envconfig.config.option`
+
+    if _skip_this_perhaps(venv):
+        return None
+
     pass
 
+
+@hookimpl
 def tox_runtest(venv: tox.venv.VirtualEnv, redirect: bool):
     """
     Args:
@@ -58,5 +82,6 @@ def tox_runtest(venv: tox.venv.VirtualEnv, redirect: bool):
         `redirect` (bool): I have no clue what this does yet
     """
 
-    envconfig:tox.config.TestenvConfig = venv.envconfig
+    if _skip_this_perhaps(venv):
+        return None
     return True
