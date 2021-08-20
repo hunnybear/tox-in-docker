b8a7cc965409cf1105e285ef0bdab8ef5081a441
Tyler Jachetta <tjachetta@imgix.com>
2021-08-20 16:25:52 -0600

    Updated tests to reflect new path

--- tests/unit/test_main.py
+++ tests/unit/test_main.py
@@ -3,11 +3,33 @@ import unittest.mock as mock
 
 import tox_in_docker.main
 
+ENV_NAME = 'my_env'
+IMAGE_TAG = 'my_image:oldest'
+
+
+def
+
+
+@mock.patch('__main__.open', mock.mock_open())
+@mock.patch('tempfile.TemporaryDirectory')
+@mock.patch('docker.client.DockerClient')
 class Test_Launch(unittest.TestCase):
 
-    @mock.patch('tox_in_docker.main.do_launch')
-    def test_do_tests(self, do_launch_mock):
-        tox_in_docker.main.do_tests()
+    def test_run_tests_explicit_image(
+            self,
+            client_class_mock,
+            temporary_directory_mock,
+            open_mock):
 
-        do_launch_mock.assert_called_once()
+        client_mock = client_class_mock.return_value
 
+        # Docker client will be created automagically
+        tox_in_docker.main.run_tests(ENV_NAME, image=IMAGE_TAG)
+
+        client_mock.containers.run.assert_called_once_with(
+            ENV_NAME,
+            image=IMAGE_TAG,
+            # might be nice to enforce dict
+            volumes=mock.ANY,
+            entrypoint=mock.ANY
+        )
