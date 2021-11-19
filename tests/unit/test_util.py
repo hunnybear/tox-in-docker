637443cabbbb9189e65931d740096f199de26eb0
Tyler Jachetta <tjachetta@imgix.com>
2021-11-19 09:08:59 -0700

    Fix syntax

--- tests/unit/test_util.py
+++ tests/unit/test_util.py
@@ -14,7 +14,7 @@ class TestGetDefaultImages(unittest.TestCase):
 
     def test_major(self):
         for env, image_base in self.BASES:
-            for version in range(1,4):
+            for version in range(1, 4):
                 print(version, str(version))
                 this_env = f'{env}{str(version)}'
                 with self.subTest(env=env, this_env=this_env, image_base=image_base):
@@ -25,8 +25,13 @@ class TestGetDefaultImages(unittest.TestCase):
     def test_minor(self):
         for env, image_base in self.BASES:
             for maj_version in [2, 3]:
-                for min_version in [1,2,12]:
+                for min_version in [1, 2, 12]:
                     this_env = f'{env}{maj_version}{min_version}'
                     with self.subTest(env=env, this_env=this_env, min_version=min_version, maj_version=maj_version):
                         res = util.get_default_image(this_env)
                         self.assertEqual(res, f'{image_base}:{maj_version}.{min_version}')
+
+    def test_jython_raises_exception(self):
+        for jython in ['jy', 'jython', 'jy27', 'jy2']:
+            with self.assertRaises(util.NoJythonSupport):
+                util.get_default_image(jython)
