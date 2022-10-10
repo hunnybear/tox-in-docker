044c6b60b4072ed974157831222e4a3a36c31984
Tyler Jachetta <me@tylerjachetta.net>
2022-10-10 09:49:46 -0600

    Tests: Updated tests for main to reflect changes from 0a1ac865537256c1b3d5b035969ba9f4b3dce000

--- tests/unit/test_main.py
+++ tests/unit/test_main.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 import unittest
 import unittest.mock as mock
@@ -47,22 +48,44 @@ class Test_Launch(unittest.TestCase):
         chmod_patch.start()
         self.addCleanup(chmod_patch.stop)
 
+        copytree_patch = mock.patch('shutil.copytree')
+        # ToDo validate calls in tests
+        self.copytree_mock = copytree_patch.start()
+        self.addCleanup(copytree_patch.stop)
+
+        # ToDo Verify with tests that link is happening
+        path_link_patch = mock.patch('pathlib.Path.symlink_to')
+        self.path_link_mock = path_link_patch.start()
+        self.addCleanup(path_link_patch.stop)
+
 
         client_class_patch = mock.patch('docker.client.from_env')
         self.client_class_mock = client_class_mock = client_class_patch.start()
         self.addCleanup(client_class_patch.stop)
 
+        reglob_patch = mock.patch('tox_in_docker.util.ReGlobPath')
+        self.reglob_mock = reglob_patch.start()
+        reglob_instance = self.reglob_mock.return_value
+        self.addCleanup(reglob_patch.stop)
+        reglob_instance.joinpath.configure_mock(return_value=reglob_instance)
+
+        # make reglob yield matches
+        reglob_instance.re_glob.configure_mock(return_value=iter(mock.MagicMock() for _i in range(2)))
+
         # set up some names for easier use
         self.client_mock = client_mock = client_class_mock.return_value
         self.run_mock = run_mock = client_mock.containers.run
-        self.container_mock = run_mock.return_value
+        self.create_container_mock = create_mock = client_mock.containers.create
+        self.container_from_run_mock = run_mock.return_value
+        self.container_from_create_mock = create_mock.return_value
 
 
         # setup default successful container run
         self._set_container_status(0)
 
     def _set_container_status(self, status: int, error: str = None) -> None:
-        self.container_mock.wait.configure_mock(return_value={'StatusCode': status, 'Error': error})
+        self.container_from_create_mock.wait.configure_mock(return_value={'StatusCode': status, 'Error': error})
+        self.container_from_run_mock.wait.configure_mock(return_value={'StatusCode': status, 'Error': error})
 
     def test_run_tests_explicit_image(
             self, temporary_directory_mock):
@@ -71,21 +94,26 @@ class Test_Launch(unittest.TestCase):
         """
 
         venv_mock = mock.Mock(spec=tox.venv.VirtualEnv())
+        venv_mock.path = '/some/path'
+        venv_mock.name = 'mock-venv-pls-ignore'
         tox_in_docker.main.run_tests(venv_mock, image=IMAGE_TAG)
 
-        self.run_mock.assert_called_once_with(
+        self.create_container_mock.assert_called_once_with(
             image=IMAGE_TAG,
             volumes={
-                temporary_directory_mock().__enter__(): {'bind': '/working_dir', 'mode': 'rw'},
-                str(Path().absolute()): {'bind': '/testing-ro', 'mode': 'ro'}
+                temporary_directory_mock().name: {'bind': os.getcwd(), 'mode': 'rw'},
+                str(Path().absolute()): {'bind': '/testing-ro', 'mode': 'ro'},
+                venv_mock.envconfig.config.toxworkdir: {'bind': '/.tox', 'mode': 'rw'}
             },
             command=['-e', AnyMock],
-            stream=True,
-            stderr=True,
-            stdout=True,
             user=AnyInt,
-            remove=True,
             detach=True
+        )
+
+        self.container_from_create_mock.start.assert_called_once_with()
+
+        self.container_from_create_mock.attach.assert_called_once_with(
+            logs=True, stdout=True, stderr=True, stream=True
         )
 
     def test_run_tests_auto_image(
@@ -94,22 +122,28 @@ class Test_Launch(unittest.TestCase):
         with mock.patch('tox_in_docker.util.get_default_image') as get_image_mock:
 
             venv_mock = mock.Mock(spec=tox.venv.VirtualEnv())
+            venv_mock.path = '/a/path/eh'
+            venv_mock.name = 'mock-venv-pls-ignore'
             venv_mock.envconfig.in_docker = True
             tox_in_docker.main.run_tests(venv_mock)
 
             get_image_mock.assert_called_once_with(venv_mock.envconfig.envname)
 
-            self.run_mock.assert_called_once_with(
+            # Todo Mock os.getcwd
+            self.create_container_mock.assert_called_once_with(
                 image=get_image_mock.return_value,
                 volumes={
-                    temporary_directory_mock().__enter__(): {'bind': '/working_dir', 'mode': 'rw'},
-                    str(Path().absolute()): {'bind': '/testing-ro', 'mode': 'ro'}
+                    temporary_directory_mock().name: {'bind': os.getcwd(), 'mode': 'rw'},
+                    str(Path().absolute()): {'bind': '/testing-ro', 'mode': 'ro'},
+                    venv_mock.envconfig.config.toxworkdir: {'bind': '/.tox', 'mode': 'rw'}
                 },
                 command=['-e', venv_mock.envconfig.envname],
-                stream=True,
-                stderr=True,
-                stdout=True,
                 user=AnyInt,
-                remove=True,
                 detach=True
+            )
+
+            self.container_from_create_mock.start.assert_called_once_with()
+
+            self.container_from_create_mock.attach.assert_called_once_with(
+                logs=True, stdout=True, stderr=True, stream=True
             )
